e8119baf79f375caf40bcde8f265c4f4502174a9
Joe Thompson <joe@pop-os.localdomain>
2019-11-05 20:14:55 -0500

    Procted routes setup with redirect and logout function works to clear local storage

--- friends/src/Components/AppRouting.js
+++ friends/src/Components/AppRouting.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Route, withRouter } from "react-router-dom";
+import ProtectedRoute from "./ProtectedRoute";
 import LoginForm from "./LoginForm";
 import FriendsList from "./FriendsList";
 import AddFriend from "./AddFriend";
+import Logout from "./Logout";
 
 function AppRouting() {
   return (
     <div>
       <Route exact path="/login" component={LoginForm} />
-      <Route exact path="/friends-list" component={FriendsList} />
-      <Route exact path="/add-friend" component={AddFriend} />
+      <ProtectedRoute exact path="/friends-list" component={FriendsList} />
+      <ProtectedRoute exact path="/add-friend" component={AddFriend} />
+      <ProtectedRoute exact path="/logout" component={Logout} />
     </div>
   );
 }
--- friends/src/Components/Nav.js
+++ friends/src/Components/Nav.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import { getToken } from "../Utils/Api";
 
 function Nav() {
 
+    const signedIn = getToken();
+
     return (
-        <div>
+        <nav>
             <Link to="/login">Login</Link>
-            <Link to={"/friends-list"}>Friends</Link>
-            <Link to={"/add-friend"}>Add Friend</Link>
-            <Link to={"/"}>Log Out</Link>
-        </div>
+            {signedIn && <Link to={"/friends-list"}>Friends</Link>}
+            {signedIn && <Link to={"/add-friend"}>Add Friend</Link>}
+            {signedIn && <Link to={"/logout"}>Log Out</Link>}
+        </nav>
     )
 
 }
 
-export default Nav;
+export default withRouter(Nav);
